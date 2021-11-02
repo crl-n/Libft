c7783721d6c32cd365d91c12db30af40a3e3030d
Carl Nysten <cnysten@c1r6p17.hive.fi>
2021-11-02 15:14:36 +0200

    ft_memcpy.c unsigned char implementation

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,25 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 12:42:37 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/02 12:45:02 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/02 15:13:47 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
-{
+#include "libft.h"
 
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char	*tdst;
+	unsigned char	*tsrc;
+
+	i = 0;
+	tdst = (unsigned char *)dst;
+	tsrc = (unsigned char *)src;
+	while (i < n)
+	{
+		tdst[i] = tsrc[i];
+		i++;
+	}
+	return (dst);
 }
