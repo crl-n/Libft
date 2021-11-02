698505a3584fd7587cf345c07dbc91bf8938bf87
Carl Nysten <cnysten@c1r6p17.hive.fi>
2021-11-02 14:10:01 +0200

    libft.h updated

--- libft.h
+++ libft.h
@@ -6,20 +6,20 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 20:57:20 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/02 12:58:12 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/02 14:08:54 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <string.h>
+# include <string.h>
 
 /* function prototypes */
+void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-int	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *ptr, int x, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_isdigit(int c);
