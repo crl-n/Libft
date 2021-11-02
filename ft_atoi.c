beeff4442780fda7359056bd95ef575fdde8ee56
Carl Nysten <cnysten@c1r6p17.hive.fi>
2021-11-02 11:29:47 +0200

    ft_atoi with test file

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,12 +6,42 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 19:31:14 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/01 19:32:20 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/02 11:17:34 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static int	ft_iswhite(int c)
+{
+	if (c == ' ')
+		return (1);
+	if (c >= '\t' && c <= '\r')
+		return (1);
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
+	int	sign;
+	int	nb;
 
-	return (0);
+	sign = 1;
+	nb = 0;
+	if (!str)
+		return (0);
+	while (ft_iswhite(*str))
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		nb *= 10;
+		nb += (int)(*str - '0');
+		str++;
+	}
+	return (nb * sign);
 }
