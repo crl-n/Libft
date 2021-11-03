0b86033519749d459009124d67775c5584283e2c
Carl Nysten <cnysten@c1r6p16.hive.fi>
2021-11-03 14:29:16 +0200

    ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:04:32 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/03 14:28:55 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	dstlen;
+
+	i = 0;
+	dstlen = ft_strlen(dst);
+	while (src[i] && dstlen + i < dstsize)
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (dstlen + ft_strlen(src));
+}	
