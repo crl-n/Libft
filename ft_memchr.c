fde57a3db03b7dfe800a8d0bea0962f02efa73da
Carl Nysten <cnysten@c1r6p16.hive.fi>
2021-11-03 11:19:56 +0200

    ft_memchr.c added

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 10:48:15 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/03 11:19:29 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *) s)[i] == (unsigned char) c)
+			return ((void *)((unsigned char *)(s + i)));
+		i++;
+	}
+	return (NULL);
+}
