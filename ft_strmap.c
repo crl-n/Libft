f2bc5c2bd09732e13502e87cf23d0de5b26e4d20
cnysten <nysten.carl@gmail.com>
2021-11-04 14:46:49 +0200

    ft_strmap.c

---
+++ ft_strmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 14:40:43 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/04 14:46:36 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	size_t	size;
+	size_t	i;
+	char	*new;
+
+	size = ft_strlen(s);
+	new = (char *) malloc(size + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		new[i] = f(s[i]);
+		i++;
+	}
+	new[size + 1] = '\0';
+	return (new);
+}
