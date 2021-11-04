560785a4ae83c3a89abf4442c91be1bd945a7171
cnysten <nysten.carl@gmail.com>
2021-11-04 14:17:01 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 20:57:20 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/02 14:08:54 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/03 16:12:14 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 # define LIBFT_H
 
 # include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 /* function prototypes */
+void	*ft_memalloc(size_t size);
+;
+void	ft_putnbr(int n);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr(char const *s);
+void	ft_putchar(char c);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 int		ft_tolower(int c);
