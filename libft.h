a6d6e4778ed07ef275e3c53552245ba4f37a2dc9
cnysten <nysten.carl@gmail.com>
2021-11-05 15:42:20 +0200

    libft.h new functions added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 20:57:20 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/03 16:12:14 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/05 11:06:24 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,24 @@
 # include <unistd.h>
 
 /* function prototypes */
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strcat(char *s1, const char *s2);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strtrim(char const *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+char	*ft_strdup(const char *s1);
 void	*ft_memalloc(size_t size);
-;
 void	ft_putnbr(int n);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putchar_fd(char c, int fd);
