c554933c6bc0ed001c6da881e758333c1016439e
cnysten <nysten.carl@gmail.com>
2021-11-05 10:26:45 +0200

    ft_strsub.c

---
+++ ft_strsub.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 10:16:13 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/05 10:26:35 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*sub;
+
+	if (len == 0)
+		return (ft_strdup(""));
+	sub = (char *) malloc((len + 1) * sizeof (char));
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + (unsigned int) i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
