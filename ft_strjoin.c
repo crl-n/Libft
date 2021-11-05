eefd1a58a8ce9c457c48c04bb5ff0138a80dcb28
cnysten <nysten.carl@gmail.com>
2021-11-05 10:47:25 +0200

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 10:27:29 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/05 10:47:15 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	size_t	size;
+	char	*join;
+
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	size = len1 + len2 + 1;
+	join = (char *) malloc(size * sizeof (char));
+	if (!join)
+		return (NULL);
+	join[size - 1] = '\0';
+	ft_strcpy(join, s1);
+	ft_strcat(join, s2);
+	return (join);
+}
