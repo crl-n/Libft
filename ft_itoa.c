a03e2d276ec8a1416a87f86da14476e25b6b05c5
cnysten <nysten.carl@gmail.com>
2021-11-07 18:13:40 +0200

    ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 16:57:15 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/07 17:59:47 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_intlen(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char * ft_itoa(int n)
+{
+	char	*s;
+	size_t	size;
+
+	size = ft_intlen(n) + 1;
+	s = (char *) malloc(size * sizeof (char));
+	if (!s)
+		return (NULL);
+	s[--size] = '\0';
+	while (size > 0)
+	{
+		s[--size] = n % 10;
+		n = n / 10;
+	}
+	return (s);
+}
