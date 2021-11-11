f9bdf2b83953a484f60f23c023ad6676174142c4
cnysten <nysten.carl@gmail.com>
2021-11-11 19:02:27 +0200

    ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,47 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:48:01 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/05 11:27:04 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/11 16:49:10 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	trimmed_len(char const *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+		s++;
+	while (*s++)
+		len++;
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+	{
+		s--;
+		len--;
+	}
+	return (len);
+}
+
 char	*ft_strtrim(char const *s)
 {
-	return (ft_strdup(s));
+	char	*trim;
+	size_t	size;
+	size_t	i;
+
+	size = trimmed_len(s);
+	trim = (char *) malloc(size * sizeof (char));
+	if (!trim)
+		return (NULL);
+	i = 0;
+	while (*s == ' ' || *s == '\t' || *s == '\n')
+		s++;
+	while (s[i] && i < size - 1)
+	{
+		trim[i] = s[i];
+		i++;
+	}
+	trim[size - 1] = '\0';
+	return (trim);
 }
