39f2c4850124cbce2cb28f447607a2cb18db2e59
cnysten <nysten.carl@gmail.com>
2021-11-11 19:02:06 +0200

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:57:15 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/07 17:59:47 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/11 17:18:06 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static size_t	ft_intlen(int n)
 	return (len);
 }
 
-char * ft_itoa(int n)
+char	*ft_itoa(int n)
 {
 	char	*s;
 	size_t	size;
@@ -39,7 +39,7 @@ char * ft_itoa(int n)
 	s[--size] = '\0';
 	while (size > 0)
 	{
-		s[--size] = n % 10;
+		s[--size] = '0' + (n % 10);
 		n = n / 10;
 	}
 	return (s);
