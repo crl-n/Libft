817928254f967ebffc4f34021a1f20734414f566
cnysten <nysten.carl@gmail.com>
2021-11-15 21:16:03 +0200

    ft_strlcat.c all tests passed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:04:32 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/12 17:05:36 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/15 21:15:47 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,23 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
+	size_t	j;
 	size_t	dstlen;
-	size_t	remsize;
+	size_t	srclen;
 
 	i = 0;
-	dstlen = 0;
-	remsize = dstsize;
-	while (dst[dstlen] && dstlen < dstsize)
-	{
-		dstlen++;
-		remsize--;
-	}
-	while (src[i] && remsize > 0)
-	{
-		dst[dstlen + i] = src[i];
+	j = 0;
+	srclen = 0;
+	while (dst[i] != '\0' && i < dstsize)
 		i++;
-		remsize--;
+	while (src[j] != '\0' && (i + j + 1 < dstsize))
+	{
+		dst[i + j] = src[j];
+		j++;
 	}
-	if (dstlen < dstsize)
-		dst[dstlen + i] = '\0';
-	return (dstlen + ft_strlen(src));
+	if (i < dstsize)
+		dst[i + j] = '\0';
+	srclen = ft_strlen(src);
+	dstlen = i;
+	return (dstlen + srclen);
 }	
