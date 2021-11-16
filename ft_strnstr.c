92d4289f14b6c2968a7fc3baf50cbb9df0d04b65
cnysten <nysten.carl@gmail.com>
2021-11-16 21:30:05 +0200

    ft_strnstr.c passes all tests

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:42:28 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/03 15:49:08 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/16 21:29:12 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,22 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 	size_t	k;
 
+	if (ft_strequ(needle, ""))
+		return ((char *) haystack);
 	i = 0;
 	while (haystack[i] && i < len)
 	{
 		j = i;
 		k = 0;
-		while (haystack[j] == needle[k])
+		if (haystack[j] == needle[k])
 		{
+			while (haystack[j] && haystack[j] == needle[k] && j < len)
+			{
+				j++;
+				k++;
+			}
 			if (needle[k] == '\0')
 				return ((char *)(haystack + i));
-			j++;
-			k++;
 		}
 		i++;
 	}
