bec6c2e9d3da36003fb9462e6c00d012f1241a33
cnysten <nysten.carl@gmail.com>
2021-11-17 15:40:53 +0200

    ft_strsub.c undefined behaviour when start >= len

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:16:13 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/05 10:36:17 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/17 15:39:15 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	if (len == 0)
 		return (ft_strdup(""));
 	sub = (char *) malloc((len + 1) * sizeof (char));
-	if (!sub)
+	if (!sub || !s)
 		return (NULL);
 	i = 0;
 	while (i < len)
