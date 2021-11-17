d2eddf46caaf62e02a8c7cab256587c8b9f81f38
cnysten <nysten.carl@gmail.com>
2021-11-17 16:37:15 +0200

    ft_strcat.c completely rewritten function, no mallco

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:20:49 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/03 13:54:21 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/17 16:35:40 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,14 @@
 char	*ft_strcat(char *s1, const char *s2)
 {
 	size_t	i;
-	size_t	len1;
-	size_t	len2;
-	char	*dst;
+	char	*ptr;
 
 	i = 0;
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	dst = (char *) malloc((len1 + len2 + 1) * sizeof (char));
-	if (!dst)
-		return (NULL);
-	while (i < len1)
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	while (i < len1 + len2)
-	{
-		dst[i] = s2[i - len1];
-		i++;
-	}
-	dst[len1 + len2] = '\0';
-	return (dst);
+	ptr = s1;
+	while (*ptr)
+		ptr++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = '\0';
+	return (s1);
 }
