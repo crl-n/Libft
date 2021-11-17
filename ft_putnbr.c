5b7d10f66234059d49ffc9cfa0d7997dfede6060
cnysten <nysten.carl@gmail.com>
2021-11-17 19:02:01 +0200

    ft_putnbr.c fixed faulty cast which caused crash

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 17:15:51 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/04 09:48:37 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/17 19:01:48 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ void	ft_putnbr(int n)
 {
 	if (n < 0)
 	{
+		if (n == -2147483648)
+		{
+			ft_putstr("-2147483648");
+			return ;
+		}
 		ft_putchar('-');
-		n = -n;
-		ft_putnbr(n);
+		n = n * (-1);
 	}
-	else if (n <= 9)
+	if (n <= 9)
 	{
 		ft_putchar((char) n + '0');
 	}
 	else if (n > 9)
 	{
 		ft_putnbr(n / 10);
-		ft_putchar((char) n % 10 + '0');
+		ft_putchar((char)(n % 10 + '0'));
 	}
 }
