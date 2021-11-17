ebcad52404662e22787fae68cab3d8bf550863aa
cnysten <nysten.carl@gmail.com>
2021-11-17 19:01:20 +0200

    ft_putnbr_fd.c fixed faulty cast which caused crash

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/04 09:50:18 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/04 09:54:01 by cnysten          ###   ########.fr       */
+/*   Created: 2021/11/17 18:56:49 by cnysten           #+#    #+#             */
+/*   Updated: 2021/11/17 18:59:28 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
 	{
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
 		ft_putchar_fd('-', fd);
-		n = -n;
-		ft_putnbr_fd(n, fd);
+		n = n * (-1);
 	}
-	else if (n <= 9)
+	if (n <= 9)
 	{
 		ft_putchar_fd((char) n + '0', fd);
 	}
 	else if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd((char) n % 10 + '0', fd);
+		ft_putchar_fd((char)(n % 10 + '0'), fd);
 	}
 }
