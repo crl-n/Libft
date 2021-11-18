2672806d78ca76468733b3a2c86a7f72cd0f1a5c
cnysten <nysten.carl@gmail.com>
2021-11-18 15:25:55 +0200

    ft_strcmp.c passes all tests, difference implementation

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:50:15 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/18 15:19:08 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/18 15:23:17 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t			i;
+	size_t	i;
 
 	i = 0;
 	while (s1[i] || s2[i])
 	{
 		if ((unsigned char) s1[i] < (unsigned char) s2[i])
-			return (-1);
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 		if ((unsigned char) s1[i] > (unsigned char) s2[i])
-			return (1);
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 		i++;
 	}
 	return (0);
