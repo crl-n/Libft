f28b5a74648185d99c8e4367c48c808619bafeea
cnysten <nysten.carl@gmail.com>
2021-11-22 16:43:05 +0200

    ft_lstadd.c passes all tests

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,17 +6,16 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 22:02:53 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/19 16:29:21 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/22 16:42:41 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "test_libft.h"
+#include "libft.h"
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	if (!alst || !new)
-	{
-		new->next = *alst;
-		*alst = new;
-	}
+	if (!new)
+		return ;
+	new->next = *alst;
+	*alst = new;
 }
