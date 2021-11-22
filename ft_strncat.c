095c36fcc76c25d349511c26104349e34b36b0ec
cnysten <nysten.carl@gmail.com>
2021-11-22 20:29:15 +0200

    ft_strncat.c passes all tests

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 13:56:50 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/03 14:03:40 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/22 20:28:57 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,16 @@ char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 	size_t	j;
-	size_t	len1;
-	size_t	len2;
-	char	*dst;
 
 	i = 0;
 	j = 0;
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	dst = (char *) malloc((len1 + len2 + 1) * sizeof (char));
-	if (!dst)
-		return (NULL);
-	while (i < len1)
-	{
-		dst[i] = s1[i];
+	while (s1[i])
 		i++;
+	while (s2[j] && j < n)
+	{
+		s1[i + j] = s2[j];
+		j++;
 	}
-	while (i < len1 + len2 && j < n)
-		dst[i++] = s2[j++];
-	dst[len1 + len2] = '\0';
-	return (dst);
+	s1[i + j] = '\0';
+	return (s1);
 }
