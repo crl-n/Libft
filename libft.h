e4c6b7c0e478ee7aec338e0326f136dea67fe8d8
cnysten <nysten.carl@gmail.com>
2021-11-22 20:59:23 +0200

    Makefile and libft.h update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 20:57:20 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/15 20:07:09 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/22 13:34:02 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,31 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}	t_list;
+
 /* function prototypes */
-int	ft_strequ(char const *s1, char const *s2);
-int	ft_strcmp(const char *s1, const char *s2);
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+int	ft_strnequ(char const *s1, char const *s2, size_t n);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_strclr(char *s);
+void	ft_strdel(char **as);
+char	*ft_strnew(size_t size);
+void	ft_memdel(void **ap);
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
+t_list	*ft_lstnew(void const *content, size_t content_size);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	**ft_strsplit(char const *s, char c);
 char	*ft_itoa(int n);
