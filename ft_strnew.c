613f5bc9bafefe24b0baf3e468f82d43b0126e90
cnysten <nysten.carl@gmail.com>
2021-11-22 18:18:25 +0200

    ft_strnew.c correct size

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 14:17:32 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/04 14:22:04 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/22 17:42:05 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_strnew(size_t size)
 	char	*str;
 	size_t	i;
 
-	str = (char *) malloc(size);
+	str = (char *) malloc(size + 1);
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (i < size)
+	while (i < size + 1)
 		str[i++] = '\0';
 	return (str);
 }
