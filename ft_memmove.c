2b55de55eb82f3b13ebc55d276c13829e7e594c6
cnysten <nysten.carl@gmail.com>
2021-11-23 18:40:09 +0200

    ft_memmove.c cost drop  fixed

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 13:29:54 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/12 13:06:50 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/23 18:34:04 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		return (dst);
 	while (i < len)
 	{
-		temp[i] = ((unsigned char *) src)[i];
+		temp[i] = ((const unsigned char *) src)[i];
 		i++;
 	}
 	i = 0;
