51df3dc67d7f05bf9f7dbfcb9d97799a66e25d11
cnysten <nysten.carl@gmail.com>
2021-11-24 08:42:56 +0200

    ft_memmove.c no malloc

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 13:29:54 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/23 18:34:04 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/11/24 08:42:02 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*temp;
 	size_t			i;
 
 	if (!dst && !src)
 		return (NULL);
 	i = 0;
-	temp = (unsigned char *) malloc(len * sizeof (unsigned char));
-	if (!temp)
-		return (dst);
-	while (i < len)
+	if (dst < src)
 	{
-		temp[i] = ((const unsigned char *) src)[i];
-		i++;
+		while (i < len)
+		{
+			((unsigned char *) dst)[i] = ((const unsigned char *) src)[i];
+			i++;
+		}
 	}
-	i = 0;
-	while (i < len)
+	else if (dst > src)
 	{
-		((unsigned char *) dst)[i] = temp[i];
-		i++;
+		while (len--)
+		{
+			((unsigned char *) dst)[len] = ((const unsigned char *) src)[len];
+		}
 	}
 	return (dst);
 }
