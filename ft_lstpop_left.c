d890d6d7a673fe30e57b584f42fe679568c31034
cnysten <nysten.carl@gmail.com>
2021-12-03 18:17:45 +0200

    ft_lstpop_left.c added missing null check

--- ft_lstpop_left.c
+++ ft_lstpop_left.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:38:40 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/23 17:45:12 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/12/03 18:17:20 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_list	*ft_lstpop_left(t_list **head)
 {
 	t_list	*lst;
 
+	if (!head)
+		return (NULL);
 	if (!*head)
 		return (NULL);
 	lst = *head;
