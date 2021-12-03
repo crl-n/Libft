8093652fc884b1b7fae1c1df16d1c1d16edac446
cnysten <nysten.carl@gmail.com>
2021-12-03 18:10:37 +0200

    ft_lstdelone.c added missing null protection

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 13:53:55 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/19 15:44:08 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/12/03 18:10:16 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 {
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+	if (!alst || !del)
+		return ;
+	if (*alst)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
 }
