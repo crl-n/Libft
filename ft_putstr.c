66035508982ac4d4a059d57e03ac00d54b79bc6e
cnysten <nysten.carl@gmail.com>
2021-12-03 18:43:46 +0200

    ft_putstr.c one write implementation

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 16:12:48 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/17 18:34:00 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/12/03 18:33:27 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@ void	ft_putstr(char const *s)
 {
 	if (!s)
 		return ;
-	while (*s)
-		ft_putchar(*s++);
+	write(1, s, ft_strlen(s));
 }
