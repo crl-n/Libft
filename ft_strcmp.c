d18f8c580da22af23f1985c78c20a60faad4f8d2
cnysten <nysten.carl@gmail.com>
2021-12-03 18:19:31 +0200

    ft_strcmp.c combined two if statements

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:50:15 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/18 15:23:17 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/12/03 18:18:36 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while (s1[i] || s2[i])
 	{
-		if ((unsigned char) s1[i] < (unsigned char) s2[i])
-			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
-		if ((unsigned char) s1[i] > (unsigned char) s2[i])
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
 			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 		i++;
 	}
