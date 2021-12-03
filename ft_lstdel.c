ddd5431da52b5f76f65b5fbfe198fd582b0de121
cnysten <nysten.carl@gmail.com>
2021-12-03 18:09:07 +0200

    ft_lstdel.c null protection added

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 15:47:53 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/19 16:23:09 by cnysten          ###   ########.fr       */
+/*   Updated: 2021/12/03 18:08:46 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 
 	if (!alst || !del)
 		return ;
-	next = (*alst)->next;
-	if (next)
-		ft_lstdel(&next, del);
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
+	if (*alst)
+	{
+		next = (*alst)->next;
+		if (next)
+			ft_lstdel(&next, del);
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
 }
