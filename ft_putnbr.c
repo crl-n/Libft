6c833f0a9958ee3bf90783f5f88ab93ef5b388cd
Carl Nysten <nysten.carl@gmail.com>
2022-07-24 00:03:42 +0300

    ft_putnbr replaced with ft_putbnr2

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 17:15:51 by cnysten           #+#    #+#             */
-/*   Updated: 2021/11/17 19:01:48 by cnysten          ###   ########.fr       */
+/*   Updated: 2022/07/24 00:02:26 by carlnysten       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 void	ft_putnbr(int n)
 {
+	char	str[12];
+	size_t	i;
+	int		sign;
+
+	if (n == -2147483648)
+		return (ft_putstr("-2147483648"));
+	ft_bzero(str, 12);
+	i = 10;
+	sign = 1;
 	if (n < 0)
 	{
-		if (n == -2147483648)
-		{
-			ft_putstr("-2147483648");
-			return ;
-		}
-		ft_putchar('-');
+		sign = -1;
 		n = n * (-1);
 	}
-	if (n <= 9)
+	while (n > 9)
 	{
-		ft_putchar((char) n + '0');
+		str[i--] = (char)(n % 10 + '0');
+		n = n / 10;
 	}
-	else if (n > 9)
-	{
-		ft_putnbr(n / 10);
-		ft_putchar((char)(n % 10 + '0'));
-	}
+	str[i] = (char)(n + '0');
+	if (sign == -1)
+		str[--i] = '-';
+	ft_putstr(str + i);
 }
